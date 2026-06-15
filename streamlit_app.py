754bff6405206bbaf9398a0f9b9d5c7d8be33af5
Natthaya W <natthaya.watthan@gmail.com>
2026-06-15 11:39:09 +0100

    Update streamlit_app.py
    add new section to display smoothiefroot nutition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,8 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon='✅')
         
-        
+
+# new section to display smoothiefroot nutition information
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
